c5768b35e79b7a1fcab58dcc033f298fc62e5fc1
Dylan James Ramsden <dylan.james.ramsden@gmail.com>
2025-03-22 22:12:57 +0200

    [ADD] Added GameplayAbilities module in Build.cs.

--- Source/Platformer/Platformer.Build.cs
+++ Source/Platformer/Platformer.Build.cs
@@ -7,7 +7,13 @@ public class Platformer : ModuleRules
 	public Platformer(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		// Public vs Private Dependancies
+		// - Public: Modules that depend on our module will also depend on the dependencies defined in public
+		// - Private: Any module that depends on our module won't depend on the dependencies defined in private
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput"});
+		
+		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayAbilities" });
 	}
 }
