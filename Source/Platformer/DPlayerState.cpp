088b8e1a6ce27187f376f82e87eb098c31c118e1
Dylan James Ramsden <dylan.james.ramsden@gmail.com>
2025-03-22 23:10:12 +0200

    [ADD] DPlayerState now has a AbilitySystemComponent. Also granting the AbilitySet.

--- Source/Platformer/DPlayerState.cpp
+++ Source/Platformer/DPlayerState.cpp
@@ -3,3 +3,31 @@
 
 #include "DPlayerState.h"
 
+#include <AbilitySystemComponent.h>
+
+#include "DAbilitySet.h"
+
+DEFINE_LOG_CATEGORY(LogDPlayerState);
+
+ADPlayerState::ADPlayerState()
+{
+	AbilitySystemComp = CreateDefaultSubobject<UAbilitySystemComponent>("AbilitySystemComp");
+}
+
+void ADPlayerState::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ensure(AbilitySystemComp);
+
+	if (AbilitySet)
+	{
+		// Instead of calling and referencing the Ability Set in the PS, might be better to handle it directly through the
+		// AbilitySystemComp
+		AbilitySet->GiveToAbilitySystem(AbilitySystemComp);
+	}
+	else
+	{
+		UE_LOG(LogDPlayerState, Warning, TEXT("AbilitySet not assigned resulting in no AbilitySet being set on the AbilitySystemComp."));
+	}
+}
--- Source/Platformer/DPlayerState.h
+++ Source/Platformer/DPlayerState.h
@@ -6,6 +6,11 @@
 #include "GameFramework/PlayerState.h"
 #include "DPlayerState.generated.h"
 
+class UAbilitySystemComponent;
+class UDAbilitySet;
+
+DECLARE_LOG_CATEGORY_EXTERN(LogDPlayerState, Log, All);
+
 /**
  * 
  */
@@ -13,5 +18,27 @@ UCLASS()
 class PLATFORMER_API ADPlayerState : public APlayerState
 {
 	GENERATED_BODY()
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly, Category = "AbilityComponent")
+	UAbilitySystemComponent* AbilitySystemComp;
+
+	UPROPERTY(EditDefaultsOnly, Category = "AbilityComponent")
+	const UDAbilitySet* AbilitySet = nullptr;
+
+public:
+
+	ADPlayerState();
+
+	/**
+	 * Gets the Ability System Component of a player state. N.B! Does not do any validation checks.
+	 * @return The player state's Ability System Component.
+	 */
+	UAbilitySystemComponent* GetAbilitySystemComponent() { return AbilitySystemComp; }
+
+protected:
+
+	virtual void BeginPlay() override;
 	
 };
